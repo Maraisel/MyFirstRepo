a19a0b4afd3b9c4315727eb84e51e8dc6d8487b4
Maraisel <68696942+Maraisel@users.noreply.github.com>
2020-07-23 10:13:24 -0400

    Added html, css & js files

---
+++ script.js
@@ -0,0 +1,15 @@
+console.clear();
+
+console.log("hello world");
+
+let myFirstVariable = 7;
+let mySecondVariable = 10;
+
+console.log(myFirstVariable + mySecondVariable)
+
+let firstname = "Maraisel"
+let lastname = "Morales"
+
+console.log(firstname+ " " + lastname);
+
+title.style.color = 'blue';
